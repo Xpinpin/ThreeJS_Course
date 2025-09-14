c1427f2d5a87c588f28282b8af2b180ef20d8641
Nguyen Pham <55543578+Xpinpin@users.noreply.github.com>
2025-09-13 23:06:36 -0300

    Add starter Three.js exercise project
    Initial commit for the Lessons/05/starter_Exercise project, including package.json, Vite config, basic HTML, JS, CSS files, and dependencies for Three.js and GSAP.

---
+++ Lessons/05/starter_Exercise/src/script.js
@@ -0,0 +1,67 @@
+import * as THREE from "three";
+import gsap from "gsap";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// //Time
+// let time = Date.now();
+
+//Clock
+const clock = new THREE.Clock();
+
+// Animations
+const tick = () => {
+  //   //time
+  //   const currTime = Date.now();
+  //   const deltaTime = currTime - time;
+  //   time = currTime;
+  //   console.log(deltaTime);
+
+  //Clock
+  const elapsedTime = clock.getElapsedTime();
+
+  //console.log(elapsedTime);
+
+  //gsap.to(mesh.position, { x: 2, duration: 1, delay: 1 });
+  //   gsap.to(mesh.position, { duration: 1, delay: 1, x: 0 });
+  //Update Objects
+  //mesh.position.x -= 0.01;
+  //   mesh.position.y += 0.01;
+  //   mesh.rotation.y += 0.001 * deltaTime;
+  camera.position.y = Math.sin(elapsedTime);
+  //   camera.position.x = Math.cos(elapsedTime);
+  //   camera.lookAt(mesh.position);
+  //Render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
